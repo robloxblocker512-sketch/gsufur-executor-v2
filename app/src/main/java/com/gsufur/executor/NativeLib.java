9e57bde5778728e441a03348c495240c33d14a6e
robloxblocker512-sketch <robloxblocker512@gmail.com>
2026-08-10 10:53:53 -0400

    Add files via upload

---
+++ app/src/main/java/com/gsufur/executor/NativeLib.java
@@ -0,0 +1,9 @@
+﻿package com.gsufur.executor;
+
+public class NativeLib {
+    static {
+        System.loadLibrary("bgfx_hook");
+    }
+    public static native int initialize();
+    public static native int executeScript(String script);
+}
