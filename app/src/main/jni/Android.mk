4d3ae143e0c24834f3a11047756a231110c77508
robloxblocker512-sketch <robloxblocker512@gmail.com>
2026-08-10 11:31:56 -0400

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,6 +1,7 @@
-﻿LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := bgfx_hook
 LOCAL_SRC_FILES := bgfx_hook.c
 LOCAL_LDLIBS := -llog -ldl
+LOCAL_CFLAGS := -std=c11
 include $(BUILD_SHARED_LIBRARY)
