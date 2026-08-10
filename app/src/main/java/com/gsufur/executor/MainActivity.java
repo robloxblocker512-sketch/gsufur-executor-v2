757605ce9b91fcc88016b93d9427f19682186d54
robloxblocker512-sketch <robloxblocker512@gmail.com>
2026-08-09 20:19:24 -0400

    Add files via upload

---
+++ app/src/main/java/com/gsufur/executor/MainActivity.java
@@ -0,0 +1,23 @@
+﻿package com.gsufur.executor;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText scriptEditor = findViewById(R.id.scriptEditor);
+        Button executeBtn = findViewById(R.id.executeBtn);
+
+        executeBtn.setOnClickListener(v -> {
+            String script = scriptEditor.getText().toString();
+            Toast.makeText(this, script.isEmpty() ? "Empty" : "Executed!", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
