bf2ab354bd7c7bc20a152d684c18bbf943407838
robloxblocker512-sketch <robloxblocker512@gmail.com>
2026-08-11 11:26:02 -0400

    Update Android.mk to include zlib and RTTI

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := bgfx_hook
 LOCAL_SRC_FILES := bgfx_hook.cpp
-LOCAL_LDLIBS := -llog -ldl -pthread
-LOCAL_CPPFLAGS := -std=c++11 -fexceptions
+LOCAL_LDLIBS := -llog -ldl -lz
+LOCAL_CPPFLAGS := -std=c++11 -fexceptions -frtti
+LOCAL_CPP_FEATURES := exceptions
+LOCAL_LDFLAGS := -lc++_static
 include $(BUILD_SHARED_LIBRARY)
